bbcd0d29949e857468a67c60c77642d997e161c6
Vlado Chabal <v.chabal@gmail.com>
2018-08-07 22:27:23 +0200

    Selector change, documentation, add cookie

--- src/detector.js
+++ src/detector.js
@@ -1,6 +1,16 @@
 window.requestAnimationFrame(function () {
-    style = document.createElement('style');
-    style.appendChild(document.createTextNode('.ytd-masthead { display:none }'));
+    // hide privacy reminder
+    var style = document.createElement('style');
+    style.appendChild(document.createTextNode('#ticker { display:none }'));
     document.body.appendChild(style);
+    
+    // resize window to calculate elements size
     window.dispatchEvent(new Event('resize'));
+
+    // set cookie that everything is ok
+    var date = new Date();
+    date.setDate(date.getDate() + 1);
+    document.cookie = "HideTicker=true;domain=youtube.com;path=/;expires=" + date.toUTCString();
 });
+
+//xk("HideTicker", "true", 86400);
